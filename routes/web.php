053be03be1aa1796e700bcc0037a910463f2ed59
cleison.paiva <cleison606@gmail.com>
2022-08-01 13:13:38 -0300

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Jobs\ExampleJob;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/test-job', function () {
+
+    ExampleJob::dispatch([
+        'example' => 'value'
+    ]);
+
+    return 'ok';
+});
+
+Route::get('/', function () {
+    return view('welcome');
+});
